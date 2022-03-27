7bef1676331b2ebac5cce9849382d8e341da315b
babtun123 <85534070+babtun123@users.noreply.github.com>
2022-03-27 14:07:46 -0500

    Create data.py

---
+++ data.py
@@ -0,0 +1,23 @@
+
+
+game_info = {"turtle" : "", "name": "DDA", "lives": 3, "level": 1, "score": 0}
+
+animation_items = [
+  # data for all game characters
+  {"type": "player", "turtle": "", "position": [-200, 0], "position_change": [0, .1], "image": "ezgif.com-gif-maker-2.gif", "radius": 15},
+
+  {"type": "harm", "turtle": "", "position": [-150, 150], "position_change": [0, -.1], "image": "ezgif.com-gif-maker-3.gif", "radius": 15},
+
+  {"type": "harm", "turtle": "", "position": [-100, -120], "position_change": [0, .1], "image": "ezgif.com-gif-maker-3.gif", "radius": 15},
+  
+  {"type": "harm", "turtle": "", "position": [-50, 90], "position_change": [0, -.1], "image": "ezgif.com-gif-maker-3.gif", "radius": 15},
+
+  {"type": "harm", "turtle": "", "position": [0, -60], "position_change": [0, .1], "image": "ezgif.com-gif-maker-3.gif", "radius": 15},
+
+  {"type": "harm", "turtle": "", "position": [50, 30], "position_change": [0, -.1], "image": "ezgif.com-gif-maker-3.gif", "radius": 15},
+
+  {"type": "harm", "turtle": "", "position": [100, -150], "position_change": [0, -.1], "image": "ezgif.com-gif-maker-3.gif", "radius": 15},
+  
+  {"type": "door", "turtle": "", "position": [150, -190], "position_change": [0, -.1], "image": "ezgif.com-gif-maker-4.gif", "radius": 15}
+  
+]
